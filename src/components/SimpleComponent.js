4203d2295648b5191f1f348c8b21b56027d808f9
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-03-21 10:49:13 -0600

    commit tree 91ca68c90dcf57425a635533dea783833060df9f

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,16 @@
 // Code SimpleComponent Here
+
+import React, {Component} from 'react'
+
+export default class SimpleComponent extends Component {
+    state = {mood: "happy"}
+
+    handleChange = () => {
+        const newMood = this.state.mood === 'happy' ? 'sad' : 'happy'
+        this.setState({mood: newMood})
+    }
+    
+    render() {
+        return <div onClick={this.handleChange}>{this.state.mood}</div>
+    }
+}
